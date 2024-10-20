bcb2b1a40c28ca7fb42023ad51184a9bcc0142e8
PAVAN SUNDAR PENDEM <141473568+pavansundar9@users.noreply.github.com>
2024-10-20 18:15:16 +0530

    Update firebaseConfig.js
    For security reasons, removed the api keys for the firebase

--- src/components/firebaseConfig.js
+++ src/components/firebaseConfig.js
@@ -7,15 +7,13 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCc-kTSQ8KhjjbMsFtNnlQi24qdbgjOTTc",
-    authDomain: "usersphere-d6472.firebaseapp.com",
-    databaseURL: "https://usersphere-d6472-default-rtdb.firebaseio.com",
-    projectId: "usersphere-d6472",
-    storageBucket: "usersphere-d6472.appspot.com",
-    messagingSenderId: "979340864681",
-    appId: "1:979340864681:web:24bf5c82cd26bb1d24cdb8",
-    measurementId: "G-MJMR06KDSQ"
-  };
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
